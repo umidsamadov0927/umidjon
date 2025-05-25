f8be9e598dd56db60637144924215f021df3c6cf
umidsamadov0927 <umidsamadov0927@gmail.com>
2025-05-26 00:03:08 +0500

    added

--- new.js
+++ new.js
@@ -1,89 +1,95 @@
-// for (let a= 10; a > -10; a--){
-//     console.log("hello")
-//
-//     for (let i = 0 ; i < 5; i++)
-//         console.log("world")
-// }
-
-// 1-misol
+// 1-msiol
 
 // function one (bir,ikki) {
-//     let a = bir
-//     let b = ikki
-//     console.log("A")
-//     for (a; a > 0; a -- ) {
+//     let a = 1;
+//     let b = 1;
+//     console.log("Birinchi sonlar")
+//     while (a <= bir) {
 //         console.log(a)
+//         a += 1
 //     }
-//     console.log("B")
-//     for (b; b > 0; b -- ) {
+//     console.log("Ikkinchi sonlar")
+//     while (b <= ikki) {
 //         console.log(b)
+//         b += 1
 //     }
-// }one (10,15)
+// }
+// one(prompt("1-sonni kiriting"), prompt("2-sonni kiriting"))
 
 
 // 2-misol
 
 // function two (sum , cycle) {
 //     let a = sum
-//     for (let i = 1; i <= cycle; i++ ) {
-//         for (let j = 1; j <= 1; j++ ) {
-//             console.log(j*i + " kg konfet")
-//         }
-//         console.log(a * i + " so'm")
+//     let b = 1
+//     while (b <= cycle) {
+//         console.log(b + " kg = " + sum * b)
+//         b += 1
 //     }
 // }
-// two (1000 , 5)
+// two (500,10)
 
 // 3-misol
 
-// function three (money) {
-//     let sum = money
-//     for (let i = 0.1; i <= 1.0; i+= 0.1) {
-//         console.log(`${i.toFixed(1)} kg = ${(sum * i ).toFixed(0)} so'm`)
+// function three (sum) {
+//     let a = 1
+//     let b = sum
+//     while (a <= 10){
+//         console.log((a/10).toFixed(1),(sum * (a/10)).toFixed(0))
+//         a++
 //     }
 // }
-// three(500)
-
+// three(1000)
 
 // 4-misol
 
-// function four (one,two,three) {
+// function four () {
+//     let a = parseInt(prompt("1-chi son"))
+//     let b = parseInt(prompt("2-chi son"))
+//     while (a <= b) {
+//         console.log(a)
+//         a += 1
+//     }
+// }
+// four()
+
+// 5-misol chalaaaaaaaaa
+
+// function five (one, two, three) {
 //     let a = one
 //     let b = two
 //     let c = three
-//     for (let i = a; i <= b; i++ ) {
-//         c += i
+//     while (i <= c) {
+//         i += 1
+//         console.log(i*c)
+//
 //     }
-//     console.log(`Yig'indisi: ${c}`)
-// } four(5, 10 , 0)
-
-
-// 5-misol
-
-// function five (one, two, three) {
-//     let b = one
-//     let  first = two
-//     let second = three
-//     for (let a = first; a <= second; a++) {
-//         b *=a
-//     }
-//     console.log(b)
-// } five(2,4,7)
-
+// }
+// five(2,4,7)
 
 // 6-misol
 
-
-// function six (one,two) {
-//     for (let a = one; a <= two; a++) {
-//         if (a % 3 === 0) {
-//             console.log("Fizz")
-//         }else if (a % 5 === 0) {
-//             console.log("Buzz")
-//         }else{
-//             console.log("FizzBuzz")
+// function solve() {
+//     let i = 1
+//     while (i <= 100) {
+//         switch (true) {
+//             case (i % 3 === 0 && i % 5 === 0 ):
+//                 console.log(i + " FizzBuzz")
+//                 break
+//             case (i % 3 ===0 ):
+//                 console.log(i + " Fizz")
+//                 break
+//             case (i % 5 === 0 ):
+//                 console.log(i + " Buzz")
+//                 break
+//             default:
+//                 console.log(i)
 //         }
+//         i++
 //     }
-// } six (5, 110)
+// }
+// solve();
+
+
 
 
